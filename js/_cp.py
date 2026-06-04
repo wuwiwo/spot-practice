308eef88634dc9bec773057ff6d5379399d0a84d
wuio-pc <wangdunhao@foxmail.com>
2026-06-04 23:43:13 +0800

    cleanup temp files

--- js/_cp.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-content = open(sys.argv[1], 'r', encoding='utf-8').read()
-with open(sys.argv[2], 'w', encoding='utf-8') as f:
-    f.write(content)
-print('Copied', len(content), 'bytes')
